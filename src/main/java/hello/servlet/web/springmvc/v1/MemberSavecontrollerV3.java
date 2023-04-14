2b74f2f9eac4f66b5e7b2a6c8db26b9d8a32ebc2
junhyukPark <junni802@naver.com>
2023-04-14 17:54:20 +0900

    추가

---
+++ src/main/java/hello/servlet/web/springmvc/v1/MemberSavecontrollerV3.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSavecontrollerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return null;
+    }
+}
